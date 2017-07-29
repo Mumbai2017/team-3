139053d623a3186ac786e75e7bee5d2e35c40882
shr1911 <shraddha.mak1911@gmail.com>
2017-07-29 21:44:17 +0530

    backend - volunteer login - localhost

---
+++ backend/db_connect.php
@@ -0,0 +1,10 @@
+<?PHP
+
+$user_name = "root";
+$pass_word = "";
+$database = "c4g";
+$server = "localhost";
+
+$conn = mysqli_connect($server, $user_name, $pass_word, $database);
+
+?>
--- backend/volunteer/volunteer_signup.php
+++ backend/volunteer/volunteer_signup.php
@@ -2,19 +2,50 @@
 require '../db_connect.php';
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST'){
-	$vid = $_POST['vid'];
-	$vcontact = $_POST['vcontact'];
-	$vaddress = $_POST['vaddress'];
-	$vcategory = $_POST['vcategory'];
-	$vemail = $_POST['vemail'];
-	$reference = $_POST['reference'];
-	$reason = $_POST['reason'];
-	$prefloc = $_POST['prefloc'];
-	$otherloc = $_POST['otherloc'];
+	// $vid = $_POST['vid'];
+	// $vname = $_POST['vname']
+	// $vcontact = $_POST['vcontact'];
+	// $vaddress = $_POST['vaddress'];
+	// $vcategory = $_POST['vcategory'];
+	// $vemail = $_POST['vemail'];
+	// $reference = $_POST['reference'];
+	// $reason = $_POST['reason'];
+	// $prefloc = $_POST['prefloc'];
+	// $otherloc = $_POST['otherloc'];
+	// $password = $_POST['password'];
 
-	$SQL = "INSERT INTO volunteer (vid, vcontact, vaddress, vcategory, vemail, reference, ) VALUES ($vid, $vcontact, $vaddress,$vcategory, $vemail, $reference, $reason, $prefloc, $otherloc)";
+	//$vid = $_POST['vid'];
+	$vname = "Shraddha";
+	$vcontact = 9773919892;
+	$vaddress = "Mumbai,India";
+	$vcategory = "abc";
+	$vemail = "shraddha.mak1911@gmail.com";
+	$reference = "abc.com";
+	$reason = "xyz";
+	$prefloc = "Mumbai";
+	$otherloc = "";
+	$password = "12345";
 
-	$result = mysqli_query($conn, $SQL);
+
+
+	$SQL = "SELECT * FROM volunteer WHERE vemail = '$vemail' ";
+            $result = mysqli_query($conn, $SQL);
+            $num_rows = mysqli_num_rows($result);
+
+            if ($num_rows > 0) {
+                $errorMessage = "Username already taken";
+            } else {
+
+                $SQL = "INSERT INTO volunteer (vcontact, vaddress, vcategory, vemail, reference, reason, prefloc, otherloc) VALUES ($vcontact, '$vaddress',$vcategory, '$vemail', $reference, '$reason', $prefloc, '$otherloc')";
+
+				$result = mysqli_query($conn, $SQL);
+
+
+                mysqli_close($conn);
+               	$errorMessage = "";
+                //header("Location: login.php");
+            }
+	
 }
 
 ?>
