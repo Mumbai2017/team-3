2ad96ca3fb994ded44c2bf45d9cf4fc195a36058
shr1911 <shraddha.mak1911@gmail.com>
2017-07-30 03:44:36 +0530

    main login page

--- backend/main_page_login.php
+++ backend/main_page_login.php
@@ -5,33 +5,40 @@ require 'db_connect.php';
 //if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 	// $uemail = $_POST['uemail'];
 	// $pwd = $_POST['pwd'];
-	// $role = $_POST['role'];
-
 	$uemail = "abc@gmail.com";
 	$pwd = "123";
-	$role = "volunteer";
 
-	$SQL = "SELECT * FROM user WHERE uemail = '$uemail' ";
+	$SQL = "SELECT pwd FROM user WHERE uemail = '$uemail' ";
     $result = mysqli_query($conn, $SQL);
     $num_rows = mysqli_num_rows($result);
 
-    if ($num_rows > 0) {
-        $x = array("success"=>false, "message"=>"Oops! User Already exists.");
+    if ($num_rows <= 0) {
+        $x = array("success"=>false, "message"=>"Oops! User does not exist.");
            	echo json_encode($x);
     }
     else {
-   	    $SQL = "INSERT INTO user (uemail, pwd, role) VALUES ('$uemail', '$pwd' ,'$role')";
-   	    echo "Hello";
-		$result = mysqli_query($conn, $SQL);
-		if($result == TRUE){
-			$x = array("success"=>true, "message"=>"Successfully Signed Up.");					
-			echo json_encode($x);
-		}else{
-            $x = array("success"=>false, "message"=>"Oops! Error registering.");
-            echo json_encode($x);
-		}
-        mysqli_close($conn);
+    	if ($pwd!=$result) {
+			$x = array("success"=>false, "message"=>"Wrong password! Please re-enter.");
+           		echo json_encode($x);
     }
+    	}
+    	else {
+    	$SQL = "SELECT role from user where uemail = '$uemail' and pwd = '$pwd' ";
+    	$result = mysqli_query($conn,$SQL);
+    	if ($result == 'volunteer')  {
+    		//header: ' Location: volunteer-login.php ';
+    	}
+    	else if ($result == 'donor')  {
+    		//header: ' Location: donor-login.php ';
+    	}
+    	else if ($result == 'doctor')  {
+    		//header: ' Location: doctor-login.php ';
+    	}
+    	else{
+
+    	}
+
+    	}    }
  //}
 
 ?>
