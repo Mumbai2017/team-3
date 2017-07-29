5c8efa8928ed879d3df076fa9115b8a6c988fce4
Archish <archishthakkar@gmail.com>
2017-07-29 22:06:21 +0530

    Authentication & Dashboard

--- MakeAWish/app/src/main/java/com/archish/makeawish/MyFirebaseInstanceIDService.java
+++ MakeAWish/app/src/main/java/com/archish/makeawish/MyFirebaseInstanceIDService.java
@@ -2,13 +2,10 @@ package com.archish.makeawish;
 
 import android.util.Log;
 
+import com.archish.makeawish.data.local.SharedPreferenceManager;
 import com.google.firebase.iid.FirebaseInstanceId;
 import com.google.firebase.iid.FirebaseInstanceIdService;
-import com.markdevelopers.rakshak.data.local.SharedPreferenceManager;
 
-/**
- * Created by Belal on 03/11/16.
- */
 
 
 public class MyFirebaseInstanceIDService extends FirebaseInstanceIdService {
