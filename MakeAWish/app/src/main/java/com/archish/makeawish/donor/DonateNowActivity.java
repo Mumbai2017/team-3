067c3065d2f8307cb4391e7dc7d0f846abd1a1c8
Archish <archishthakkar@gmail.com>
2017-07-30 09:00:47 +0530

    Final Commit Coming Soon

---
+++ MakeAWish/app/src/main/java/com/archish/makeawish/donor/DonateNowActivity.java
@@ -0,0 +1,19 @@
+package com.archish.makeawish.donor;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.archish.makeawish.R;
+
+/**
+ * Created by Archish on 7/30/2017.
+ */
+
+public class DonateNowActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_donor_2);
+    }
+}
